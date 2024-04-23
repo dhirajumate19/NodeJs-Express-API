02b8f446a3a5ff7658567cc35bb361346ad5b3ee
Dhiraj Umate <dhiraju1999@gmail.com>
2024-04-23 08:08:01 +0530

    Added Rate limit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import { rateLimit } from "express-rate-limit";
 import userRouter from "./Features/Users/User.routes.js";
 
 //Declraring Port
@@ -14,11 +15,22 @@ mongoose
     console.log("Db not Connected", err);
   });
 
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 15 minutes
+  limit: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  // store: ... , // Redis, Memcached, etc. See below.
+  message: {
+    message: "You have exceeded the API limit. Please contact admin",
+  },
+});
 //creates an instance of an Express application,
 // allowing you to build web applications using the Express framework in Node.js.
 const app = express();
 // sets up a rule for your Express application to understand and handle data sent to it in JSON format.
 app.use(express.json());
+app.use(limiter);
 app.use("/user", userRouter);
 
 //This starts the Express application and instructs it to listen for incoming
